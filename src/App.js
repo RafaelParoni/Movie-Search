0f70819746dd4286857e60b3be68f988661f6fcf
Rafael Paroni <rafael.paronis.oficial@gmail.com>
2023-08-30 16:58:23 -0300

    adicionando segunda página na lista de filmes
    adicionando segunda página na lista de filmes e opção de alterar entre elas

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {GrLanguage} from 'react-icons/gr';
 import {GrChannel} from 'react-icons/gr';
 import {GrTrophy} from 'react-icons/gr';
 import {GrValidate} from 'react-icons/gr';
+import {CiBookmark} from 'react-icons/ci'
 // import Icons ----|
 
 import './style.css';
@@ -16,12 +17,19 @@ import api from './services/api'
 import posterError from './poster.jpg'
 var movie = ''
 var errorValue = true;
-var filmes = []
+var Pages1 = []
+var Pages2 = []
+var btnPages = []
+var ValuePage = 1
+var value = ''
 function App() {
   const [input, setInput] = useState('');
   const [film, setFilm] = useState({});
   const [filmInfo, setFilmInfo] = useState({});
 
+
+
+ 
 
   async function handleSearch(){
     //  12237837/json/
@@ -31,27 +39,58 @@ function App() {
       return;
     }
     try{
-      filmes = []
-      var value = input.toLowerCase()
+      Pages1 = []
+      Pages2 = []
+      value = input.toLowerCase()
       value = value.trim()
       const response = await api.get(`?s=${value}&apikey=c74f3650&plot=full`);
 
       setFilm(response.data.Search);
-
       for(const key in response.data.Search){
-        if(key > 5){break}
-        filmes.push(
-          <a><div className='movie' onClick={() => movieDetais(response.data.Search[key])}>
-          <img src={response.data.Search[key].Poster} />
-          <p>{response.data.Search[key].Title}</p>
-          </div></a>)
+        if(key > 4){break}
+        Pages1.push(
+              <div className='movies'>
+                  <div className='movie' onClick={() => movieDetais(response.data.Search[key])}>
+                  <img src={response.data.Search[key].Poster} />
+                  <p>{response.data.Search[key].Title}</p>
+                  </div>
+                </div>
+          )
       };
+      for(const key in response.data.Search){
+        if(key > 10){break}
+        if(key < 5){console.log('Lugar errado'+key)}else if(key < 10){
+          Pages2.push(
+                <div className='movies'>
+                    <div className='movie' onClick={() => movieDetais(response.data.Search[key])}>
+                    <img src={response.data.Search[key].Poster} />
+                    <p>{response.data.Search[key].Title}</p>
+                    </div>
+                  </div>
+            )
+        }
+        
+      };
+      
+
+      var i = 1
+      btnPages = []
+      while( i <= 2){
+        btnPages.push(
+          <>
+          <span className='BtnPages' id={'btn-page' + i} onClick={() => page(i)}></span>
+          </>
+        )
+        i++
+      }
       if(response.data.Response == 'False'){errorValue = false; setFilm({}); movie = input }else {errorValue = true}
+
       setInput('');
     }catch{
       errorValue = false
       setInput('');
       setFilm({});
+      alert('erro')
     }
 
     async function movieDetais(value){
@@ -69,13 +108,35 @@ function App() {
         
       }
     }
+    async function page(){
+      var pagina1 =  document.getElementById('page1')
+      var pagina2 =  document.getElementById('page2')
+      if(ValuePage == 1){ // page 2
+        document.getElementById(`btn-page2`).style.backgroundColor = '#089dd8'
+        document.getElementById('pagesText').innerHTML = '2'
+        document.getElementById('btn-page1').style.backgroundColor = 'transparent'
+        pagina1.style.display = 'none'
+        pagina2.style.display = 'flex'
+      
+        ValuePage = 2
+      }else{ // page 1
+        document.getElementById(`btn-page1`).style.backgroundColor = '#089dd8'
+        document.getElementById('pagesText').innerHTML = '1'
+        document.getElementById('btn-page2').style.backgroundColor = 'transparent'
+        pagina2.style.display = 'none'
+        pagina1.style.display = 'flex'
+       
+        ValuePage = 1
+      }
+    }
+ 
+
 
 
   }return (
     <div className="center">
       
       <h1>Search a movie:</h1>
-
       <div className="containerInput" id='teste'>
           <input 
           type="text"
@@ -94,10 +155,19 @@ function App() {
       </div>
       {Object.keys(film).length > 0 &&( // Buscar Filmes
         <div className='results'>
-          <div className='movies'>
-            {filmes}
+          <span className='pages' > <CiBookmark />Page: <a id='pagesText'>{ValuePage} </a></span>
+          <a>
+            <a id='page1'>
+              {Pages1}
+            </a>
+            <a id='page2'>
+              {Pages2}
+            </a>
+          </a>
+          <div className='DivPages'>
+            {btnPages}
           </div>
-      </div>
+         </div>  
       )}
       {errorValue == false && ( // Error ao buscar Filmes
           <main className='resultError'>
